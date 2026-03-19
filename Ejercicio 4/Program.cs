5a69f8522304a8073e85e59cac2b23dd6c84b658
Adri4 <justinlopezlucas14@gmail.com>
2026-03-19 17:38:29 -0600

    Sin error

---
+++ Ejercicio 4/Program.cs
@@ -0,0 +1,9 @@
+﻿int anio;
+while (true)
+{
+    Console.Write("Año de nacimiento (1900-2026): ");
+    if (int.TryParse(Console.ReadLine(), out anio) && anio >= 1900 && anio <= 2026) break;
+    {
+        Console.WriteLine("Error: Año fuera de rango lógico.");
+    }
+}
