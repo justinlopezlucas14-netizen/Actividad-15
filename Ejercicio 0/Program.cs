23083e9fc6c8e79c3e7d0f261dff139c51e1af9b
ALUMNO <ALUMNO@DESKTOP-BJ12FTC>
2026-03-19 11:14:06 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio 0/Program.cs
@@ -0,0 +1,10 @@
+﻿int edad;
+Console.Write("Ingrese su edad: ");
+if (int.TryParse(Console.ReadLine(), out edad))
+{
+    Console.WriteLine("Edad válida");
+}
+else
+{
+    Console.WriteLine("Edad invalidada");
+}
