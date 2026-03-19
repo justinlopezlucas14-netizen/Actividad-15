6f70327460f56c7e9282f0aa21ef7a3d6f12c8b4
Adri4 <justinlopezlucas14@gmail.com>
2026-03-19 17:48:10 -0600

    Sin error

---
+++ Ejercicio 5/Program.cs
@@ -0,0 +1,11 @@
+﻿int num;
+while (true)
+{
+    Console.Write("Ingrese un número entero: ");
+    if (int.TryParse(Console.ReadLine(), out num))
+    {
+        Console.WriteLine(num % 2 == 0 ? "Es Par" : "Es Impar");
+        break;
+    }
+    Console.WriteLine("Dato incorrecto.");
+}
---
+++ Ejercicio 6/Program.cs
@@ -0,0 +1,11 @@
+﻿DateTime fecha;
+while (true)
+{
+    Console.Write("Ingrese fecha(dd/mm/aaaa): ");
+    if (DateTime.TryParse(Console.ReadLine(), out fecha))
+    {
+        Console.WriteLine($"Fecha corta: {fecha.ToShortDateString()}");
+        break;
+    }
+    Console.WriteLine("Formato de fecha inválido.");
+}
