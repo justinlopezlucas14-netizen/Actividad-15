752771ff67a026c441b22813c4ba74430d559819
Adri4 <justinlopezlucas14@gmail.com>
2026-03-19 17:07:26 -0600

    Le agregue un while hasta que el usuriao quiera detenerse

---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,7 @@
+﻿decimal precio;
+while (true) 
+{
+    Console.Write("Ingrese el precio: ");
+    if (decimal.TryParse(Console.ReadLine(), out precio) && precio > 0) break;
+    Console.WriteLine("Error: Debe ser un número decimal mayor a 0.");
+}
