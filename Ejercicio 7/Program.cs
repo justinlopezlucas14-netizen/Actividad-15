4c2522b03e1fba5e34643aeaa0164af554d4a200
Adri4 <justinlopezlucas14@gmail.com>
2026-03-19 18:00:10 -0600

    Sin error

---
+++ Ejercicio 7/Program.cs
@@ -0,0 +1,9 @@
+﻿double salario;
+while (true)
+{
+    Console.Write("Ingrese salario: ");
+    if (double.TryParse(Console.ReadLine(), out salario) && salario >= 0) break;
+    {
+        Console.WriteLine("Error: El salario no puede ser negativo.");
+    }
+}
---
+++ Ejercicio 8/Program.cs
@@ -0,0 +1,11 @@
+﻿double valor;
+while (true)
+{
+    Console.Write("Número para raíz cuadrada: ");
+    if (double.TryParse(Console.ReadLine(), out valor) && valor >= 0)
+    {
+        Console.WriteLine($"Raíz: {Math.Sqrt(valor)}");
+        break;
+    }
+    Console.WriteLine("Error: Ingrese un número no negativo.");
+}
