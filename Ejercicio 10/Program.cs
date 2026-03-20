ba7f37b15f46ce1be191996b12892c706d03c80a
Adri4 <justinlopezlucas14@gmail.com>
2026-03-19 18:06:38 -0600

    Sin error

---
+++ Ejercicio 10/Program.cs
@@ -0,0 +1,15 @@
+﻿int cant;
+while (true)
+{
+    Console.Write("Cantidad de productos a comprar: ");
+    if (int.TryParse(Console.ReadLine(), out cant) && cant > 0)
+    {
+        for (int i = 1; i <= cant; i++)
+        {
+            Console.Write($"Ingrese nombre del producto {i}: ");
+            Console.ReadLine();
+        }
+        break;
+    }
+    Console.WriteLine("Ingrese un número entero positivo.");
+}
