60005eb9c65458544b6850b464bf06f928f87665
Adri4 <justinlopezlucas14@gmail.com>
2026-03-19 18:02:18 -0600

    Sin error

---
+++ Ejercicio 9/Program.cs
@@ -0,0 +1,11 @@
+﻿float nota;
+while (true)
+{
+    Console.Write("Nota del estudiante (0-100): ");
+    if (float.TryParse(Console.ReadLine(), out nota) && nota >= 0 && nota <= 100)
+    {
+        Console.WriteLine(nota >= 60 ? "Aprobado" : "Reprobado");
+        break;
+    }
+    Console.WriteLine("Nota inválida.");
+}
